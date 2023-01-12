aadfe9e5332ba408b2fb93190d06b9ba8b5f4d23
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-01-12 16:40:06 +0400

    01/12 v2

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import HelloWorld from "./components/01-hello-world/hello-world";
+import Jsx1 from "./components/03-jsx/jsx1";
+import Jsx2 from "./components/03-jsx/jsx2";
+import Jsx3 from "./components/03-jsx/jsx3";
+import Jsx4 from "./components/03-jsx/jsx4";
+import Jsx5 from "./components/03-jsx/jsx5";
+import Jsx6 from "./components/03-jsx/jsx6";
+import Style1 from "./components/04-styles/style1";
+import Style2 from "./components/04-styles/style2";
+import Style3 from "./components/04-styles/style3";
+import Style4 from "./components/04-styles/style4";
+import Clock1 from "./components/05-clock1/clock1";
+import Greetings from "./components/06-props-1/greetings";
+import Products from "./components/07-props-2/products";
+import Clock2 from "./components/08-clock2/clock2";
+import Gallery from "./components/09-image/gallery";
+import Image from "./components/09-image/image";
+import ProfileCard from "./components/10-profile-card/profile-card";
+import BootstrapDynamic from "./components/11-bootstrap/bootstrap-dynamic";
+import BootstrapStatic from "./components/11-bootstrap/bootstrap-static";
+import Icon from "./components/12-icons/icon";
+import Events from "./components/13-events/events";
+import Shop from "./components/14-products/shop";
+import Stateless from "./components/15-stateless/stateless";
+import Counter from "./components/16-counter/counter";
+import Birthday from "./components/17-birthday/birthday";
+
+const App = () => {
+  return (
+    <div>
+      
+
+    </div>
+  );
+};
+export default App;
