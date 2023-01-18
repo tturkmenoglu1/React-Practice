f0b621fcd14c1b5e3f47fc9417169334ed846e24
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-01-18 18:15:33 +0400

    01/18

--- src/components/shopping-cards/product-card/product-card.jsx
+++ src/components/shopping-cards/product-card/product-card.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, FormLabel } from 'react-bootstrap'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
-const ProductCard = ({title,image,price,discount,rate}) => {
+const ProductCard = ({ title, image, price, discount, rate }) => {
+  
+  const rateCal = () => {
+    let a;
+    for (let i = 1; i <= 5; i++) {
+      a = a + (rate >= i ? <AiFillStar /> : <AiOutlineStar/>);
+    }
+    return a
+  }
+
   return (
     <Card style={{ width: '18rem' }} className="">
+      <FormLabel>{discount}</FormLabel>
       <Card.Img variant="top" src={image} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text></Card.Text>
+        <Card.Title text-center>{title}</Card.Title>
+        <Card.Text>{rateCal()}</Card.Text>
+        <Card.Text>{price}</Card.Text>
       </Card.Body>
     </Card>
   )
